a36416c54d015c74b5061c6d653d603d0da2e684
holl3 <matthias.holsten@gmail.com>
2017-01-06 19:19:59 +0100

    zsh

---
+++ configFiles.js
@@ -0,0 +1,10 @@
+{
+    "userFiels": {
+        "zsh": ".zshrc"
+    }
+    "suFiels": {
+        "environment": "/etc/environment",
+        "bashrc" : "/etc/bash.bashrc",
+        "apt": "/etc/apt/apt.conf.d/95proxies"
+    }
+}
